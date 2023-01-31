b77f740269ae6a5bc4312ad8b4db22c753b0da9a
tomonakar <tomohisa.nak@gmail.com>
2023-01-31 14:03:25 +0900

    ラムダ式でリスナーを記述

--- my_first_android/app/src/main/java/com/example/my_first_android/MainActivity.kt
+++ my_first_android/app/src/main/java/com/example/my_first_android/MainActivity.kt
@@ -8,20 +8,42 @@ import android.widget.TextView
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    // 愚直にリスナークラスを定義
+//    private inner class MyListener: View.OnClickListener {
+//        override fun onClick(p0: View?) {
+//            val txtResult = findViewById<TextView>(R.id.txtResult)
+//            txtResult.text = Date().toString()
+//        }
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnCurrent = findViewById<Button>(R.id.btnCurrent)
-        btnCurrent.setOnClickListener(
-            object: View.OnClickListener {
-                override fun onClick(p0: View?) {
-                    val txtResult = findViewById<TextView>(R.id.txtResult)
-                    txtResult.text = Date().toString()
-                }
-            }
-        )
+        // リスナークラスを利用
+        // btnCurrent.setOnClickListener(MyListener())
+
+//        // オブジェクト式で定義
+//        btnCurrent.setOnClickListener(
+//            // オブジェクト式
+//            object: View.OnClickListener {
+//                override fun onClick(p0: View?) {
+//                    val txtResult = findViewById<TextView>(R.id.txtResult)
+//                    txtResult.text = Date().toString()
+//                }
+//            }
+//        )
+
+        /**
+         * ラムダ式で定義
+         */
+        btnCurrent.setOnClickListener {
+            val txtResult = findViewById<TextView>(R.id.txtResult)
+            txtResult.text = Date().toString()
+        }
     }
+
 //    fun btnCurrentClick(v: View) {
 //        val txtResult = findViewById<TextView>(R.id.txtResult)
 //        println(v)
