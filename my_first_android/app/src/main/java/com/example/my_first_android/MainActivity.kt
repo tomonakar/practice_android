79b4f35f9694845c8690ee3c4eec62278b87181a
tomonakar <tomohisa.nak@gmail.com>
2023-02-01 09:32:19 +0900

    toast作成

--- my_first_android/app/src/main/java/com/example/my_first_android/MainActivity.kt
+++ my_first_android/app/src/main/java/com/example/my_first_android/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.my_first_android.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +23,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.btnCurrent.setOnClickListener {
             binding.txtResult.text = Date().toString()
+            /**
+             * トーストオブジェクトの生成
+             */
+            val toast = Toast.makeText(this,
+            Date().toString(),Toast.LENGTH_LONG)
+            toast.show()
         }
+    }
+
+    /**
+     * 画面が破棄される前に状態を保存
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        // 画面が破棄される時に呼び出される
+        super.onSaveInstanceState(outState)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        outState.putString("txtResult", txtResult.text.toString())
+    }
+
+    /**
+     * 画面が復元される前に状態を取り出し
+     * Bundleはアプリの状態を key/valueの値で管理するオブジェクト
+     */
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        // 画面が再生成される時に呼び出される
+        super.onRestoreInstanceState(savedInstanceState)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        txtResult.text = savedInstanceState.getString("txtResult")
     }
 
 
